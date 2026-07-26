044f75776424b10b7ec4e4a8aa083336d6a2d2b9
Krishnakant Sharma <krishnaksharma.official@gmail.com>
2026-07-27 01:19:26 +0530

    Till Displaying Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,27 @@ import { useState } from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  function increase(){
-    setCount(count+1);
-  }
-  function decrease(){
-    if (count>0) {
-      setCount(count-1);    
-    }
-  }
-  return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={decrease}>-</button>
-      {" "}
-      <button onClick={increase}>+</button>
-    </div>
-  );
+  const heading = "NextTask";
+  const [tasks, setTasks] = useState([
+  "Learn React",
+  "Complete Java Assignment",
+  "Go to Gym"
+]);
+
+const [newTask, setNewTask] = useState("");
+
+return(
+  <div>
+    <h1>{heading}</h1>
+    <input type="text"></input>
+    <button>Add</button>
+    {tasks.map((task, index)=>{
+      return <p key={index}>{task}</p>
+    })}
+  </div>
+);
+
 }
+
 
 export default App;
